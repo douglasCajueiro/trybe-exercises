8e8df7101e2d9325befbac0b65b3479cabc2b0f6
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-10-23 19:11:04 -0300

    3-biggest finished

---
+++ exercises/4-JavaScript_Introduction_and_Programming_Logic/4.1-JavaScript-First-Steps/3-Biggest/biggest.js
@@ -0,0 +1,14 @@
+let value1 = 2;
+let value2 = 3;
+let value3 = 4;
+
+if (value1 > value2 && value1 > value3) {
+    console.log(value1);
+
+} else
+if (value2 > value1 && value2 > value3) {
+    console.log(value2);
+
+} else {
+    console.log(value3);
+}
