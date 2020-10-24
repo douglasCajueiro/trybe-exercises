176ce17897457b9398ce5d4d1dd31313ef2343d7
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-10-24 01:04:36 -0300

    code corrected and simplified

--- exercises/4-JavaScript_Introduction_and_Programming_Logic/4.1-JavaScript-First-Steps/11-Salary/salary.js
+++ exercises/4-JavaScript_Introduction_and_Programming_Logic/4.1-JavaScript-First-Steps/11-Salary/salary.js
@@ -1,6 +1,5 @@
 let grossSalary = 1500.10;
-let taxes = 0.00;
-let netSalary = 0.00;
+let inss, baseSalary, ir, netSalary;
 
 if (grossSalary <= 0) {
     console.log("Invalid salary, please retype it.");
@@ -9,35 +8,37 @@ if (grossSalary <= 0) {
 
     // Calculating INSS
     if (grossSalary <= 1556.94) {
-        taxes = (grossSalary / 100) * 8; // 8% for INSS
+        inss = (grossSalary / 100) * 8; // 8% for INSS
 
     } else
-    if (grossSalary >= 1556.95 || grossSalary <= 2594.92) {
-        taxes = (grossSalary / 100) * 9; // 9% for INSS
+    if (grossSalary <= 2594.92) {
+        inss = (grossSalary / 100) * 9; // 9% for INSS
 
     } else
-    if (grossSalary >= 2594.93 || grossSalary <= 5189.82) {
-        taxes = (grossSalary / 100) * 11; // 11% for INSS
+    if (grossSalary <= 5189.82) {
+        inss = (grossSalary / 100) * 11; // 11% for INSS
 
     } else { // Above 5189.82
-        taxes = 570.88; // maximun rate
+        inss = 570.88; // maximun rate
     }
     //--------------------------------
 
+    baseSalary = grossSalary - inss;
+
     // Calculating IR
-    if (grossSalary <= 1903.98) {
+    if (baseSalary <= 1903.98) {
         // No IR
 
     } else
-    if (grossSalary >= 1903.99 || grossSalary <= 2826.65) {
-        taxes += (grossSalary / 100) * 7.5; // 7.5% for IR
+    if (baseSalary <= 2826.65) {
+        taxes += (baseSalary / 100) * 7.5; // 7.5% for IR
 
     } else
-    if (grossSalary >= 2826.66 || grossSalary <= 3751.05) {
-        taxes += (grossSalary / 100) * 15; // 15% for IR
+    if (baseSalary <= 3751.05) {
+        taxes += (baseSalary / 100) * 15; // 15% for IR
 
     } else { // Above 4664.68
-        taxes += (grossSalary / 100) * 22.5; // 22.5% for IR
+        taxes += (baseSalary / 100) * 22.5; // 22.5% for IR
 
     }
 
