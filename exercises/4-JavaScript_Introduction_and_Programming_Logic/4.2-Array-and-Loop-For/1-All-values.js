af4c475d2a4992993815caa95d5b6fb77c07becd
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-10-27 15:56:44 -0300

    1-All-values added and finished

---
+++ exercises/4-JavaScript_Introduction_and_Programming_Logic/4.2-Array-and-Loop-For/1-All-values.js
@@ -0,0 +1,9 @@
+/* 
+1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let value = 0; value < numbers.length; value += 1) {
+    console.log(numbers[value]);
+}
