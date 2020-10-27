f73d04bdcc52c79767b654b7440e31d2660bd14a
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-10-27 16:03:36 -0300

    8-Array-25 added and finished

---
+++ exercises/4-JavaScript_Introduction_and_Programming_Logic/4.2-Array-and-Loop-For/8-Array-25.js
@@ -0,0 +1,11 @@
+/*
+8 - Utilizando for, crie uma array que vá de 1 até 25 e imprima o resultado;
+*/
+
+let numbersArray = [];
+
+for (var i = 1; i <= 25; i++) {
+    numbersArray.push(i);
+}
+
+console.log(numbersArray);
