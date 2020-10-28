dc9b1a95d6cb4b9d1a251ca7621ef0f19211f4f4
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-10-28 01:36:34 -0300

    4-pyramid - added rules for even numbers

--- exercises/4-JavaScript_Introduction_and_Programming_Logic/4.3-Programming-Logic-and-Algorithms/4-pyramid.js
+++ exercises/4-JavaScript_Introduction_and_Programming_Logic/4.3-Programming-Logic-and-Algorithms/4-pyramid.js
@@ -9,7 +9,7 @@ n = 5
 
 -------------------------------------*/
 
-let size = 20;
+let size = 11;
 
 
 let height = (size + 1) / 2;
@@ -17,21 +17,60 @@ let spacing = (size + 1) / 2;
 let asterisks = 1;
 let row = "";
 
-for (let degree = 0; degree < height; degree += 1) {
+if (size % 2 !== 0) {
 
-    for (let space = spacing; space > 0; space -= 1) {
-        row += " ";
+    //Rules for odd numbers
+    for (let degree = 0; degree < height; degree += 1) {
+
+        for (let space = spacing; space > 0; space -= 1) {
+            row += " ";
+
+        }
+
+        for (let asterisk = 0; asterisk < asterisks; asterisk += 1) {
+            row += "*";
+
+        }
+
+        console.log(row);
+        asterisks += 2;
+        spacing -= 1;
+        row = "";
 
     }
 
-    for (let asterisk = 0; asterisk < asterisks; asterisk += 1) {
-        row += "*";
+
+} else {
+
+    //Rules for even numbers
+
+    //Height of the Pyramid
+    height = size / 2;
+
+    //Number of asterisks in the firts row
+    asterisks = 2;
+
+    //Spacing in the firts row
+    spacing = size / 3;
+
+    for (let degree = 0; degree < height; degree += 1) {
+
+        for (let space = spacing; space > 0; space -= 1) {
+            row += " ";
+
+        }
+
+        for (let asterisk = 0; asterisk < asterisks; asterisk += 1) {
+            row += "*";
+
+        }
+
+        console.log(row);
+        asterisks += 2;
+        spacing -= 1;
+        row = "";
 
     }
 
-    console.log(row);
-    asterisks += 2;
-    spacing -= 1;
-    row = "";
 
 }
