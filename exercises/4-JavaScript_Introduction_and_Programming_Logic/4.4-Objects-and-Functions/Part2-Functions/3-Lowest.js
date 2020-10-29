f1680ffd497b4d2fdbfa50abcfa09676395bc12f
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-10-29 12:52:01 -0300

    Lowest commented

--- exercises/4-JavaScript_Introduction_and_Programming_Logic/4.4-Objects-and-Functions/Part2-Functions/3-Lowest.js
+++ exercises/4-JavaScript_Introduction_and_Programming_Logic/4.4-Objects-and-Functions/Part2-Functions/3-Lowest.js
@@ -11,17 +11,30 @@ Valor esperado no retorno da função: 6.
 
 function lowest(array) {
 
+    //Storing the current lowest number 
+    //(It stores the first value)
     let lowNum = array[0]
+
+    //Storing the current lowest number's index
     let lowIndex = 0
 
+    //Running across the array
+    //(It starts on the index 1, that will be compared to the index 0)
     for (let num = 1; num < array.length; num += 1) {
+
+        //Comparing the current value to the current lowest number
         if (array[num] < lowNum) {
+
+            //Storing its number
             lowNum = array[num];
+
+            //Storing its index
             lowIndex = num;
         }
 
     }
 
+    //Returning the index of the lowest number found in the array
     return lowIndex;
 }
 
