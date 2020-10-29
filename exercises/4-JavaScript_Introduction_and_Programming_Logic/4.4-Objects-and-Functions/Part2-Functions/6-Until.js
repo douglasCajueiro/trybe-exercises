6825617a9a5a1bcb310c1c362721e7feab7f0644
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-10-29 13:11:30 -0300

    Until commented

--- exercises/4-JavaScript_Introduction_and_Programming_Logic/4.4-Objects-and-Functions/Part2-Functions/6-Until.js
+++ exercises/4-JavaScript_Introduction_and_Programming_Logic/4.4-Objects-and-Functions/Part2-Functions/6-Until.js
@@ -13,15 +13,28 @@ Valor esperado no retorno da função: 1+2+3+4+5 = 15.
 
 function until(n) {
 
+    //Storing the String that will be returned
+    //It starts with "1", and will concatenate "+2", "+3"...
     let sumString = "1";
+
+    //Storing the total until now
     let total = 1;
 
+    //Running across the number, until n
     for (let num = 2; num <= n; num += 1) {
+
+        //Sum the current nummber to the total
         total += num;
+
+        //Concatenate a "+ current number" to the String
         sumString += "+" + num.toString();
 
     }
+
+    //Concatenating the String with sums, and its total
     sumString += " = " + total;
+
+    //Returning a String containing the sums and its total
     return (sumString);
 }
 
