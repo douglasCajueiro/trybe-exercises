f3fc08096c3383a8745b974bc4922725679edbe0
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-10-29 12:45:16 -0300

    Highest commented

--- exercises/4-JavaScript_Introduction_and_Programming_Logic/4.4-Objects-and-Functions/Part2-Functions/2-Highest.js
+++ exercises/4-JavaScript_Introduction_and_Programming_Logic/4.4-Objects-and-Functions/Part2-Functions/2-Highest.js
@@ -13,17 +13,30 @@ Valor esperado no retorno da função: 4.
 
 function highest(array) {
 
+    //Storing the current highest number 
+    //(It stores the first value)
     let topNum = array[0]
+
+    //Storing the current highest number's index
     let topIndex = 0
 
+    //Running across the array
+    //(It starts on the index 1, that will be compared to the index 0)
     for (let num = 1; num < array.length; num += 1) {
+
+        //Comparing the current value to the current highest number
         if (array[num] > topNum) {
+
+            //Storing its number
             topNum = array[num];
+
+            //Storing its index
             topIndex = num;
         }
 
     }
 
+    //Returning the highest number's index in the array
     return topIndex;
 }
 
