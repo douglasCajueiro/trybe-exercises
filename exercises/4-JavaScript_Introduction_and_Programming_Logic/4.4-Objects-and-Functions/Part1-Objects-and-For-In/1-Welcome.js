0422247be4806c8ca57d39b56bc86ccc76f3b3b5
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-10-29 00:07:11 -0300

    1-Welcome added and finished

---
+++ exercises/4-JavaScript_Introduction_and_Programming_Logic/4.4-Objects-and-Functions/Part1-Objects-and-For-In/1-Welcome.js
@@ -0,0 +1,21 @@
+/*---------------------------------------------------
+
+Imprima no console uma mensagem de boas-vindas para a personagem abaixo, incluindo seu nome.
+
+
+Valor esperado no console:
+Bem-vinda, Margarida
+
+---------------------------------------------------*/
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+
+//Solution:
+//---------------------------------------------------
+
+console.log("Bem-vinda, " + info.personagem);
