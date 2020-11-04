2aa2efd611180797d2d2ce12f5d20261a0f332c6
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-11-04 19:15:38 -0300

    fridays added

--- exercises/5-JavaScript_DOM_Events_and_Web_Storage/5.3-JavaScript-Events/script.js
+++ exercises/5-JavaScript_DOM_Events_and_Web_Storage/5.3-JavaScript-Events/script.js
@@ -31,7 +31,9 @@ function createAllDays() {
   // Month days
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   // Holidays
-  const dezHolidays = [24, 25, 31];
+	let dezHolidays = [24, 25, 31];
+	// Fridays
+	const dezFridays = [4, 11, 18, 25];
 
   const ulDays = document.getElementById('days');
 
@@ -42,8 +44,12 @@ function createAllDays() {
     liDay.innerHTML = currentDay;
 
     if (dezHolidays.indexOf(currentDay) !== -1) {
-      liDay.className = 'day holiday';
-    }
+      liDay.className += ' holiday';
+		}
+		
+		if (dezFridays.indexOf(currentDay) !== -1) {
+liDay.className += ' friday'			
+		}
 
     ulDays.appendChild(liDay);
   }
