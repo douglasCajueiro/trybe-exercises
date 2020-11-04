cf5e3b6ff609ef0f7f521d57ed18eb855f20de07
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-11-04 19:01:22 -0300

    days of the month added

--- exercises/5-JavaScript_DOM_Events_and_Web_Storage/5.3-JavaScript-Events/script.js
+++ exercises/5-JavaScript_DOM_Events_and_Web_Storage/5.3-JavaScript-Events/script.js
@@ -1,5 +1,13 @@
 function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDays = [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
+  ];
   const weekDaysList = document.querySelector('.week-days');
 
   for (let index = 0; index < weekDays.length; index += 1) {
@@ -8,9 +16,28 @@ function createDaysOfTheWeek() {
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
-  };
-};
+  }
+}
 
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+// Exercise-1
+// --------------------------------------------------------------------------------------------------------
+function createAllDays() {
+  //prettier-ignore
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  const ulDays = document.getElementById('days');
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let liDay = document.createElement('li');
+    let currentDay = dezDaysList[index];
+    liDay.className = 'day';
+    liDay.innerHTML = currentDay;
+    ulDays.appendChild(liDay);
+  }
+}
+
+createAllDays();
