4ce4708e309a2c80cd50f0aa80998d2abb3f4f58
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-11-24 18:39:21 -0300

    Part-1 Finished

--- exercises/7-Introduction_to_JavaScript_ES6_and_Unit_Tests/7.2-JavaScript-ES6-Objects/After-Lecture/Part-1.js
+++ exercises/7-Introduction_to_JavaScript_ES6_and_Unit_Tests/7.2-JavaScript-ES6-Objects/After-Lecture/Part-1.js
@@ -1,10 +1,10 @@
 const order = {
-  name: 'Rafael Andrade',
-  phoneNumber: '11-98763-1416',
+  name: "Rafael Andrade",
+  phoneNumber: "11-98763-1416",
   address: {
-    street: 'Rua das Flores',
-    number: '389',
-    apartment: '701',
+    street: "Rua das Flores",
+    number: "389",
+    apartment: "701",
   },
   order: {
     pizza: {
@@ -15,19 +15,19 @@ const order = {
       pepperoni: {
         amount: 1,
         price: 20,
-      }
+      },
     },
     drinks: {
       coke: {
-        type: 'Coca-Cola Zero',
+        type: "Coca-Cola Zero",
         price: 10,
         amount: 1,
-      }
+      },
     },
     delivery: {
-      deliveryPerson: 'Ana Silveira',
+      deliveryPerson: "Ana Silveira",
       price: 5,
-    }
+    },
   },
   payment: {
     total: 60,
@@ -36,14 +36,17 @@ const order = {
 
 const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
+  const message = `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`;
 
-}
+  console.log(message);
+};
 
 customerInfo(order);
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
-
-}
+  const message = `Olá ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza)} e ${order.order.drinks.coke.type} é R$${order.payment.total},00`;
+  console.log(message);
+};
 
 orderModifier(order);
