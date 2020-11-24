2877d412041035ab1357d8fcbdb976d43d1dcf2f
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-11-24 19:09:07 -0300

    Part-2: Exercise 1 Finished

--- exercises/7-Introduction_to_JavaScript_ES6_and_Unit_Tests/7.2-JavaScript-ES6-Objects/After-Lecture/Part-2.js
+++ exercises/7-Introduction_to_JavaScript_ES6_and_Unit_Tests/7.2-JavaScript-ES6-Objects/After-Lecture/Part-2.js
@@ -1,19 +1,34 @@
 const lesson1 = {
-  materia: 'Matemática',
+  materia: "Matemática",
   numeroEstudantes: 20,
-  professor: 'Maria Clara',
-  turno: 'manhã',
+  professor: "Maria Clara",
+  turno: "manhã",
 };
 
 const lesson2 = {
-  materia: 'História',
+  materia: "História",
   numeroEstudantes: 20,
-  professor: 'Carlos',
+  professor: "Carlos",
 };
 
 const lesson3 = {
-  materia: 'Matemática',
+  materia: "Matemática",
   numeroEstudantes: 10,
-  professor: 'Maria Clara',
-  turno: 'noite',
+  professor: "Maria Clara",
+  turno: "noite",
 };
+
+// Exercise 1
+//-------------------------------------------------
+const addKey = (object, newKey, value) => (object[newKey] = value);
+
+addKey(lesson2, "turno", "tarde");
+console.log(lesson2);
+/* 
+{
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+  turno: 'tarde'
+}
+*/
