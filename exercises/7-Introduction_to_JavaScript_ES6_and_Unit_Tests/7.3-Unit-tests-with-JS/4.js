b079ca9f1a7fefc0d9abd5c337b8a01cc1429460
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-11-25 20:45:18 -0300

    Exercise-4 Finished

--- exercises/7-Introduction_to_JavaScript_ES6_and_Unit_Tests/7.3-Unit-tests-with-JS/4.js
+++ exercises/7-Introduction_to_JavaScript_ES6_and_Unit_Tests/7.3-Unit-tests-with-JS/4.js
@@ -9,3 +9,27 @@ function myFizzBuzz(num) {
 }
 
 // implemente seus testes aqui
+assert.strictEqual(
+  myFizzBuzz(15),
+  "fizzbuzz",
+  "It must return 'fizzbuzz' when the number is divisible by 3 and 5"
+); // 1
+
+assert.strictEqual(
+  myFizzBuzz(6),
+  "fizz",
+  "It must return 'fizz' when the number is divisible only by 3"
+); // 2
+
+assert.strictEqual(
+  myFizzBuzz(10),
+  "buzz",
+  "It must return 'buzz' when the number is divisible only by 5"
+); // 3
+
+const testNum = 7;
+assert.strictEqual(
+  myFizzBuzz(testNum),
+  testNum,
+  "It must return the provided number when it is not divisible by 3 nor 5"
+);
