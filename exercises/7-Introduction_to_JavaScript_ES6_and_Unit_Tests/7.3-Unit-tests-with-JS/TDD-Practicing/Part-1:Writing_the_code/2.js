19d2ec8e3e4eaae47d1d8679c98306bb91aa9736
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-11-25 22:48:36 -0300

    TDD Part1-2 Finished

--- exercises/7-Introduction_to_JavaScript_ES6_and_Unit_Tests/7.3-Unit-tests-with-JS/TDD-Practicing/Part-1:Writing_the_code/2.js
+++ exercises/7-Introduction_to_JavaScript_ES6_and_Unit_Tests/7.3-Unit-tests-with-JS/TDD-Practicing/Part-1:Writing_the_code/2.js
@@ -1,9 +1,22 @@
-const assert = require('assert');
-// escreva a função wordLengths aqui
+const assert = require("assert");
 
-const words = ['sun', 'potato', 'roundabout', 'pizza'];
+// escreva a função wordLengths aqui
+const wordLengths = (array) => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    let currentLength = 0;
+    for (let ind = 0; ind < array[i].length; ind += 1) {
+      currentLength += 1;
+    }
+    newArray[i] = currentLength;
+  }
+  return newArray;
+};
+//----------------------------------
+
+const words = ["sun", "potato", "roundabout", "pizza"];
 const expected = [3, 6, 10, 5];
 
-assert.strictEqual(typeof wordLengths, 'function');
+assert.strictEqual(typeof wordLengths, "function");
 const output = wordLengths(words);
 assert.deepStrictEqual(output, expected);
