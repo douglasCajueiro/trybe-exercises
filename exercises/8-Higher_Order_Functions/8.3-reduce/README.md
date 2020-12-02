9bd9c2270b57c8bdbc25808ba26cfe646f91371e
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-12-02 18:16:52 -0300

    exercise-1 finished

--- exercises/8-Higher_Order_Functions/8.3-reduce/README.md
+++ exercises/8-Higher_Order_Functions/8.3-reduce/README.md
@@ -4,3 +4,6 @@
 
 **1) Dada uma matriz de matrizes, transforme em uma única matriz.**
 
+Solução: [exercise-1.js](./exercise-1.js)
+
+<hr>
--- exercises/8-Higher_Order_Functions/8.3-reduce/exercise-1.js
+++ exercises/8-Higher_Order_Functions/8.3-reduce/exercise-1.js
@@ -1,16 +1,8 @@
+const assert = require("assert");
 
-const assert = require('assert');
+const arrays = [["1", "2", "3"], [true], [4, 5, 6]];
 
-const arrays = [
-    ["1", "2", "3"],
-    [true],
-    [4, 5, 6]
-];
-
-
-function flatten() {
-  // escreva seu código aqui
-  
-}
+// escreva seu código aqui
+const flatten = () => arrays.reduce((acc, curr) => acc.concat(curr), []);
 
 assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
