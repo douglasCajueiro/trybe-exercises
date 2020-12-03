81a66c4cd13877095ad603be9f3d7ef539caf3b5
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-12-03 20:18:25 -0300

    exercise-2 finished

--- exercises/8-Higher_Order_Functions/8.4-spread-rest-destructuring/exercise-2.js
+++ exercises/8-Higher_Order_Functions/8.4-spread-rest-destructuring/exercise-2.js
@@ -1,9 +1,11 @@
-const assert = require('assert')
+const assert = require("assert");
 
 // escreva sum abaixo
 
-assert.strictEqual(sum(), 0)
-assert.strictEqual(sum(1), 1)
-assert.strictEqual(sum(1, 2), 3)
-assert.strictEqual(sum(1, 2, 3), 6)
-assert.strictEqual(sum(1, 2, 3, 4), 10)
+const sum = (...numbers) => numbers.reduce((total, num) => total + num, 0);
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
