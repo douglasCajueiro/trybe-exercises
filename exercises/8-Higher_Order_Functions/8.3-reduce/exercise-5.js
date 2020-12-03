5d23157606f5e7d1f235a0df317827e40e9fb650
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-12-02 21:27:31 -0300

    exercise-5 finished

--- exercises/8-Higher_Order_Functions/8.3-reduce/exercise-5.js
+++ exercises/8-Higher_Order_Functions/8.3-reduce/exercise-5.js
@@ -1,16 +1,24 @@
-
-const assert = require('assert');
+const assert = require("assert");
 
 const names = [
-  'Aanemarie',  'Adervandes',   'Akifusa',
-  'Abegildo',   'Adicellia',    'Aladonata',
-  'Abeladerco', 'Adieidy',  'Alarucha',
+  "Aanemarie",
+  "Adervandes",
+  "Akifusa",
+  "Abegildo",
+  "Adicellia",
+  "Aladonata",
+  "Abeladerco",
+  "Adieidy",
+  "Alarucha",
 ];
-
-
 
 function containsA() {
   // escreva seu código aqui
+  return names.reduce((acc, name) =>
+  acc + name.split("").reduce((acc, char) => {
+        if (char === "A" || char === "a") return acc + 1;
+        return acc;
+      }, 0), 0);
 }
 
 assert.deepStrictEqual(containsA(), 20);
