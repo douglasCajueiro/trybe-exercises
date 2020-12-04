0a6b0574524942f1b63c622ba91fa331a3bcbe2d
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-12-03 21:01:48 -0300

    exercise-4 finished

--- exercises/8-Higher_Order_Functions/8.4-spread-rest-destructuring/exercise-4.js
+++ exercises/8-Higher_Order_Functions/8.4-spread-rest-destructuring/exercise-4.js
@@ -1,36 +1,45 @@
 const assert = require('assert')
 
 const people = [
-    {
-        name: "Nicole",
-        bornIn: 1992,
-        nationality: "Australian"
-    },
-    {
-        name: "Harry",
-        bornIn: 2008,
-        nationality: "Australian"
-    },
-    {
-        name: "Toby",
-        bornIn: 1901,
-        nationality: "Australian"
-    },
-    {
-        name: "Frida",
-        bornIn: 1960,
-        nationality: "Dannish"
-    },
-    {
-        name: "Fernando",
-        bornIn: 2001,
-        nationality: "Brazilian"
-    }
-]
+  {
+    name: "Nicole",
+    bornIn: 1992,
+    nationality: "Australian",
+  },
+  {
+    name: "Harry",
+    bornIn: 2008,
+    nationality: "Australian",
+  },
+  {
+    name: "Toby",
+    bornIn: 1901,
+    nationality: "Australian",
+  },
+  {
+    name: "Frida",
+    bornIn: 1960,
+    nationality: "Dannish",
+  },
+  {
+    name: "Fernando",
+    bornIn: 2001,
+    nationality: "Brazilian",
+  },
+];
 
 // escreva filterPeople abaixo
 
-const filteredPeople = filterPeople(people)
+const filterPeople = (peopleArray) => {
+
+  const isCentury20 = (year) => year >= 1901 && year <= 2000;
+  
+  return peopleArray.filter(({bornIn, nationality}) =>
+  nationality === "Australian" && isCentury20(bornIn)
+  );
+};
+
+const filteredPeople = filterPeople(people);
 
 assert.deepStrictEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
 assert.deepStrictEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
