9072f5203593697b96d04f4649f8c0f9cd29b3a8
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-12-03 21:32:31 -0300

    exercise-6 finished

--- exercises/8-Higher_Order_Functions/8.4-spread-rest-destructuring/exercise-6.js
+++ exercises/8-Higher_Order_Functions/8.4-spread-rest-destructuring/exercise-6.js
@@ -6,6 +6,8 @@ const chiron = ["Chiron", "Bugatti", 2016]
 
 // escreva toObject abaixo
 
+const toObject = ([name, brand, year]) => ({name, brand, year});
+
 assert.deepStrictEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
 assert.deepStrictEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
 assert.deepStrictEqual(toObject(chiron), { name: "Chiron", brand: "Bugatti", year: 2016 })
