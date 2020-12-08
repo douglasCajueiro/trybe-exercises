b972625c3e6bc04bee05716b6342b0a43e322d7e
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-12-08 19:19:22 -0300

    exercise-4 finished

--- exercises/9-JavaScript_Async_and_Promisses/9.1-Async-and-Callbacks/exercise-4.js
+++ exercises/9-JavaScript_Async_and_Promisses/9.1-Async-and-Callbacks/exercise-4.js
@@ -6,6 +6,10 @@ const getMarsTemperature = () => {
 };
 
 // crie a função sendMarsTemperature abaixo
-
+const sendMarsTemperature = () =>
+  setTimeout(() => 
+    console.log(` Mars temperature is: ${getMarsTemperature()} degree Celsius`),
+    messageDelay()
+  );
 
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
