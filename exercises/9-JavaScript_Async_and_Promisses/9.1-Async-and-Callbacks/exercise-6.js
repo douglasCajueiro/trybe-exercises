43e93b2692b405d42db5c29cb072a99ec3a0345a
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2020-12-08 21:48:40 -0300

    exercise-6 finished

--- exercises/9-JavaScript_Async_and_Promisses/9.1-Async-and-Callbacks/exercise-6.js
+++ exercises/9-JavaScript_Async_and_Promisses/9.1-Async-and-Callbacks/exercise-6.js
@@ -12,7 +12,14 @@ const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now 
 const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
 
 // definição da função sendMarsTemperature...
-
+const sendMarsTemperature = (onSuccess, onError) => {
+  const currentTemperature = getMarsTemperature();
+  const messageSuccessfullySent = Math.random() <=0.6;
+  setTimeout(() => {
+    if(messageSuccessfullySent) onSuccess(currentTemperature)
+    else onError('Robot is busy');
+  }, messageDelay());
+};
 
 // imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
 sendMarsTemperature(temperatureInFahrenheit, handleError);
