9d340f60c54c5bd2ff6bfca66b5da969904efb04
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-01-12 14:54:46 -0300

    11.1 - Exercises done

--- exercises/11-Introduction-to-React/11.1-React-Intro/my-first-react-app/src/App.js
+++ exercises/11-Introduction-to-React/11.1-React-Intro/my-first-react-app/src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from './Component'
+
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const appointments = ['to spend time with my family', 'to solve puzzles','to juggle'];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Title />
+        </div>
+        <ul>
+          { task('to study')}
+          { appointments.map(currTask => task(currTask))}
+        </ul>
       </header>
     </div>
   );
---
+++ exercises/11-Introduction-to-React/11.1-React-Intro/my-first-react-app/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello! I'm Douglas</h1>
+        <h2>These are my daily tasks:</h2>
+      </div>
+    );
+  }
+}
+
+export default Title;
