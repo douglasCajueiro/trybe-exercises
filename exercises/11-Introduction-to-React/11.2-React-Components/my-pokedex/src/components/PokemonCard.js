0ed6c6d6b536dfb3a0371297245f4cc6e0bd9699
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-01-13 16:19:22 -0300

    Pokemons IDs added to the cards

--- exercises/11-Introduction-to-React/11.2-React-Components/my-pokedex/src/components/PokemonCard.js
+++ exercises/11-Introduction-to-React/11.2-React-Components/my-pokedex/src/components/PokemonCard.js
@@ -3,7 +3,7 @@ import {colors} from '../assets/colors';
 
 class PokemonCard extends React.Component {
   render() {
-    const {name, type, averageWeight, image} = this.props.pokemons;
+    const {name, type, averageWeight, image, id} = this.props.pokemons;
     const {value, measurementUnit} = averageWeight;
 
     return (
@@ -15,6 +15,7 @@ class PokemonCard extends React.Component {
         </div>
         
         <div className="pokemon-description">
+          <p className="pokemon-id"> #{id} </p>
           <p className="pokemon-name"> {name} </p>
           <p className="pokemon-type"> {type} </p>
           <p> Av. weight: {value} {measurementUnit} </p>
