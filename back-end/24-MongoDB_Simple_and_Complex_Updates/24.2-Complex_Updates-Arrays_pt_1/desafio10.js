5152f8a943463d5c3972a1d1007738592297bdde
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-05-05 20:30:27 -0300

    Challenge 10 done

---
+++ back-end/24-MongoDB_Simple_and_Complex_Updates/24.2-Complex_Updates-Arrays_pt_1/desafio10.js
@@ -0,0 +1,19 @@
+db.movies.updateOne(
+  { title: "Batman" },
+  { $push: {
+      cast:{
+        $each: [
+          {
+            "character": "Batman"
+          },
+          {
+            "character": "Alfred"
+          },
+          {
+            "character": "Coringa"
+          }
+        ]
+      }
+    } 
+  },
+);
