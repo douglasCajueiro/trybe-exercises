cb27acb171efd7b9cb023e853aff47597d0fb53c
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-05-05 20:30:27 -0300

    Challenge 12 done

---
+++ back-end/24-MongoDB_Simple_and_Complex_Updates/24.2-Complex_Updates-Arrays_pt_1/desafio12.js
@@ -0,0 +1,13 @@
+db.movies.updateOne(
+  { title: "Batman",
+    "cast.character": "Batman"
+  },
+  { $push: {
+      "cast.$.actor": {
+        $each: ["Michael Keaton" , "Val Kilmer", "George Clooney"],
+        $sort: 1
+      }
+    }, 
+  },
+);
+
